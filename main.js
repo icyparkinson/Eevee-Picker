cab07f14971bdb73791b586ba7d7fe36942bcd7b
koulight <koulight@hotmail.com>
2022-02-22 17:17:55 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,85 @@
+document.querySelector("#fire").addEventListener("click", flareon)
+
+function flareon(){
+    document.querySelector("img").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/136.png"
+    document.querySelector("h1").innerText="Flareon"
+    document.querySelector("body").style.backgroundColor="rgb(223	134	88 	)"
+    document.querySelector("body").style.color="rgb(241	225	166	)"
+}
+
+document.querySelector("#water").addEventListener("click", vaporeon)
+
+function vaporeon(){
+    document.querySelector("img").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/134.png"
+    document.querySelector("h1").innerText="Vaporeon"
+    document.querySelector("body").style.backgroundColor="rgb(139	201	220	)"
+    document.querySelector("body").style.color="rgb(67	110	140	)"
+}
+
+document.querySelector("#electric").addEventListener("click", jolteon)
+
+function jolteon(){
+    document.querySelector("img").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/135.png"
+    document.querySelector("h1").innerText="Jolteon"
+    document.querySelector("body").style.backgroundColor="rgb(245	215	127	)"
+    document.querySelector("body").style.color="rgb(88	57	120	)"
+}
+
+
+document.querySelector("#psychic").addEventListener("click", espeon)
+
+function espeon(){
+    document.querySelector("img").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/196.png"
+    document.querySelector("h1").innerText="Espeon"
+    document.querySelector("body").style.backgroundColor="rgb(212	187	204	)"
+    document.querySelector("body").style.color="rgb(160	71	83	)"
+}
+
+
+document.querySelector("#dark").addEventListener("click", umbreon)
+
+function umbreon(){
+    document.querySelector("img").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/197.png"
+    document.querySelector("h1").innerText="Umbreon"
+    document.querySelector("body").style.backgroundColor="rgb(88	94	106	)"
+    document.querySelector("body").style.color="rgb(237	215	156	)"
+}
+
+
+document.querySelector("#grass").addEventListener("click", leafeon)
+
+function leafeon(){
+    document.querySelector("img").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/470.png"
+    document.querySelector("h1").innerText="Leafeon"
+    document.querySelector("body").style.backgroundColor="rgb(145	164	133	)"
+    document.querySelector("body").style.color="rgb(90	72	43	)"
+}
+
+
+document.querySelector("#ice").addEventListener("click", glaceon)
+
+function glaceon(){
+    document.querySelector("img").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/471.png"
+    document.querySelector("h1").innerText="Glaceon"
+    document.querySelector("body").style.backgroundColor="rgb(193	225	222	)"
+    document.querySelector("body").style.color="rgb(62	101	107	)"
+}
+
+
+document.querySelector("#fairy").addEventListener("click", sylveon)
+
+function sylveon(){
+    document.querySelector("img").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/700.png"
+    document.querySelector("h1").innerText="Sylveon"
+    document.querySelector("body").style.backgroundColor="rgb(238	193	196	)"
+    document.querySelector("body").style.color="rgb(106	123	174	)"
+}
+
+document.querySelector("#normal").addEventListener("click", eevee)
+
+function eevee(){
+    document.querySelector("img").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png"
+    document.querySelector("h1").innerText="Eevee"
+    document.querySelector("body").style.backgroundColor="rgb(244	228	192	)"
+    document.querySelector("body").style.color="black"
+}
